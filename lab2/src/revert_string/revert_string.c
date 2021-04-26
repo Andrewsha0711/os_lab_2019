f71886696e8e9375941687f5b933c737a17b64b8
andrewsha <andrewdecker07@mail.ru>
2021-04-26 15:15:14 +0000

    Done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include <string.h> 
 
 void RevertString(char *str)
 {
-	// your code here
+	char *start = str; // указатель на начало строки
+    char *end = str + strlen(str) - 1; // указатель на конец строки
+    
+    while (start < end)  // выполнится когда дойдем до середины 
+    {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+
 }
 
